289d745259aa85568d5fbbca80155d96fad01501
Benjamin Sahlgren <bensahlgren@gmail.com>
2021-12-09 15:44:23 +0100

    added comments to api.js and EmailService.js

--- API/api.js
+++ API/api.js
@@ -1,13 +1,17 @@
 const express = require("express");
-
 const router = express.Router();
 
+/*The two variables "Post" and "Mail" are set to target the two different mongooose-schemas
+which are defined in the models-directory.*/
 const Post = require("../models/Post");
-
 const Mail = require("../models/Mail");
 
 const { postAddedEmail } = require("../services/EmailService");
 
+
+/*This router.post method is matched with the /newmail-url. It contains the variable "newMail" 
+which sends a new mail by collecting the four properties values that are declared when a user 
+fills each input field in contact.html. */ 
 router.post("/newmail", (req, res) => {
   console.log(req.body);
   const newMail = new Mail({
@@ -16,6 +20,14 @@ router.post("/newmail", (req, res) => {
     title: req.body.title,
     content: req.body.content,
   });
+
+  /* This function is set to save an error parameter. If an error would occur while sending the 
+  mail it will respond with status code 500, and a message is sent stating that an error occured 
+  and the boolean msgError will be set to true.
+  
+  Else the property values (req.body) will be set as a parameter in the postAddedEmail function 
+  which is declared in the EmailService.js-file. It will respond with status code 201 and a message 
+  is sent stating that the mail was successfully sent, and the boolean msgError is set to false. */
   newMail.save((err) => {
     if (err) {
       console.log(err);
@@ -34,12 +46,23 @@ router.post("/newmail", (req, res) => {
   });
 });
 
+
+/*This router.post method is matched with the /newpost-url. It contains the variable "newPost" which creates 
+a newly added post by collecting the two properties values that are declared when a user fills each input 
+field in index.html. */ 
 router.post("/newpost", (req, res) => {
   console.log(req.body);
   const newPost = new Post({
     title: req.body.title,
     content: req.body.content,
   });
+
+  /* This function is set to save an error parameter. If an error would occur while saving a post 
+  it will respond with status code 500, and a message is sent stating that an error occured and 
+  the boolean msgError will be set to true.
+  
+  Else it will respond with status code 201 and a message is sent stating that the post was 
+  successfully created, and the boolean msgError is set to false. */
   newPost.save((err) => {
     if (err) {
       console.log(err);
@@ -57,6 +80,12 @@ router.post("/newpost", (req, res) => {
   });
 });
 
+
+/*This router.get method is matched with the /getposts url and attempts to find Post documents.
+Unless an error occurs it will respond with status code 200, and will add the documents to the posts-property.
+
+If an error occurs while finding the documents it will respond with status code 500, and a message
+will be sent stating that an error occured and the msgError boolean will be set to true. */ 
 router.get("/getposts", (req, res) => {
   Post.find({}, (err, documents) => {
     if (err) {
@@ -66,12 +95,18 @@ router.get("/getposts", (req, res) => {
           msgError: true,
         },
       });
-    } else {
       res.status(200).json({ posts: documents });
     }
   });
 });
 
+/* The router.put function is matched with the /updatepost/(specific id) url. When calling the function
+it will look through the posts to find the specific post-id, and update it's property values in accordance
+to the changed value.
+
+It also contains an if-condition which checks if an error occurs. If error is true it will respond
+with a status code 500 followed by a message stating that an error occured, and the boolean msgError 
+will be set to true. */
 router.put("/updatepost/:id", (req, res) => {
   Post.findByIdAndUpdate(
     req.params.id,
@@ -84,6 +119,9 @@ router.put("/updatepost/:id", (req, res) => {
             msgError: true,
           },
         });
+
+/*Else it will respond with status code 200 and a message is sent stating that the post was 
+successfully updated, and the boolean msgError is set to false. */
       } else {
         res.status(200).json({
           message: { msgBody: "Post successfully updated", msgError: false },
@@ -92,6 +130,14 @@ router.put("/updatepost/:id", (req, res) => {
     }
   );
 });
+
+
+/* 
+The router.delete function is matched with the /deletepost/(specific id) url. When calling the function
+it will look after the specific post-id and delete it.
+
+The if-condition checks if an error occurs. If error is true it will respond with a status code 500 
+followed by a message stating that an error occured, and the boolean msgError will be set to true.*/
 
 router.delete("/deletepost/:id", (req, res) => {
   Post.findByIdAndDelete(req.params.id, (err) => {
@@ -102,6 +148,8 @@ router.delete("/deletepost/:id", (req, res) => {
           msgError: true,
         },
       });
+/*Else it will respond with status code 200 and a message is sent stating that the post was 
+successfully deleted, and the boolean msgError is set to false. */
     } else {
       res.status(200).json({
         message: { msgBody: "Post successfully deleted", msgError: false },
--- services/EmailService.js
+++ services/EmailService.js
@@ -9,17 +9,19 @@ const transport = nodemailer.createTransport(
   })
 );
 
-/*here we create a function that uses our transport variable to send a 
-a certain type of email*/
-const postAddedEmail = (post) => {
+/*
+This function is operating sending of mail. It has a parameter called "mail" which is set in the 
+newMail function in api.js. 
+*/
+const postAddedEmail = (mail) => {
   transport
     .sendMail({
       from: "Benjamin <bensahlgren@gmail.com>",
-      to: `${post.firstname} <${post.email}>`,
+      to: `${mail.firstname} <${mail.email}>`,
       subject: "Message received",
       text: `Hi, your message has been received. We will get back to you soon!`,
       html: `<h1>Your message has been received</h1>
-                <p>Hi ${post.firstname}, your message (${post.title}) has been received</p>
+                <p>Hi ${mail.firstname}, your message (${mail.title}) has been received</p>
                 <p>We will get back to you soon!</p>
                 <p>Have a great day!</p>`,
     })
